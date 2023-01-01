827e814e3ed7c07cef8f4812ad77b139a28c69a5
$Peaches from $Cans <kurdt94@gmail.com>
2023-01-01 05:28:24 +0100

    Add files via upload

---
+++ volume.js
@@ -0,0 +1,30 @@
+var old_url = '';
+var gain_val = 2;
+var hooked = false; 
+
+var mutationObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+            if (location.href != old_url && hooked == false) {
+                old_url = location.href;
+            if(document.querySelector('video') != null ){
+		if(!window.boosterGainNode) {
+          		const video = document.querySelector('video');
+          		const audioCtx = new AudioContext();
+	  		const mediaSource = audioCtx.createMediaElementSource(video);        
+	  		const gainNode = audioCtx.createGain();
+	  		mediaSource.connect(gainNode);
+          		gainNode.connect(audioCtx.destination);
+          		window.boosterGainNode = gainNode;
+    		}
+			window.boosterGainNode.gain.value = parseFloat(gain_val);
+			hooked = true;
+		}
+            }
+    });
+});
+
+mutationObserver.observe(document.documentElement, {childList: true, subtree: true});
+
+
+
+
